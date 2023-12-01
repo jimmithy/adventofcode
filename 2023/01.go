fd64718fc42bc91685b280e027f1db5ba21410c6
James O'Brien <james.obrien@hinge.co>
2023-12-01 15:34:17 -0500

    Part two sucked.

--- 2023/01.go
+++ 2023/01.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -20,6 +21,11 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
+	// partOne(scanner)
+	partTwo(scanner)
+}
+
+func partOne(scanner *bufio.Scanner) {
 	total := 0
 
 	for scanner.Scan() {
@@ -41,4 +47,68 @@ func main() {
 	}
 
 	fmt.Println(total)
+}
+
+func partTwo(scanner *bufio.Scanner) {
+	total := 0
+
+	replacer := strings.NewReplacer(
+		"one", "1",
+		"two", "2",
+		"three", "3",
+		"four", "4",
+		"five", "5",
+		"six", "6",
+		"seven", "7",
+		"eight", "8",
+		"nine", "9",
+	)
+
+	backwardsReplacer := strings.NewReplacer(
+		"eno", "1",
+		"owt", "2",
+		"eerht", "3",
+		"ruof", "4",
+		"evif", "5",
+		"xis", "6",
+		"neves", "7",
+		"thgie", "8",
+		"enin", "9",
+	)
+
+	for scanner.Scan() {
+		raw := scanner.Text()
+		line := replacer.Replace(raw)
+
+		re := regexp.MustCompile("[0-9]")
+		matches := re.FindAllString(line, -1)
+
+		first := matches[0]
+
+		backwards := Reverse(raw)
+		backwardsLine := backwardsReplacer.Replace(backwards)
+		backwardsMatches := re.FindAllString(backwardsLine, -1)
+
+		last := backwardsMatches[0]
+
+		matchesNumeric, err := strconv.Atoi(first + "" + last)
+
+		if err == nil {
+			total = total + matchesNumeric
+		} else {
+			fmt.Println("Error: " + err.Error())
+		}
+	}
+
+	fmt.Println(total)
+}
+
+func Reverse(s string) string {
+	var reverse string
+
+	for i := len(s) - 1; i >= 0; i-- {
+		reverse = reverse + string(s[i])
+	}
+
+	return reverse
 }
