86d49183c3a9c294dc79232f58c49481c1ed482b
James O'Brien <jobrienapps@gmail.com>
2024-12-07 20:56:29 -0500

    2024 Day One Part 1
    I don't have enough time to do this kind of thing this year
    
    ### To Run
    
    ```
    go run 01.go
    ```

---
+++ 2024/01.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("01.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	partOne(scanner)
+	// partTwo(scanner)
+}
+
+func partOne(scanner *bufio.Scanner) {
+	total := 0
+	left := []int{}
+	right := []int{}
+
+	// Read the file and split the numbers into two lists
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), "   ")
+
+		leftNumber, leftErr := strconv.Atoi(line[0])
+
+		if leftErr == nil {
+			left = append(left, leftNumber)
+		}
+
+		rightNumber, rightErr := strconv.Atoi(line[1])
+
+		if rightErr == nil {
+			right = append(right, rightNumber)
+		}
+	}
+
+	// Sort the two lists
+	sort.Ints(left)
+	sort.Ints(right)
+
+	for i := 0; i < len(left); i++ {
+		if left[i] > right[i] {
+			total = total + (left[i] - right[i])
+		} else {
+			total = total + (right[i] - left[i])
+		}
+	}
+
+	fmt.Println(total)
+}
+
+func partTwo(scanner *bufio.Scanner) {
+	total := 0
+
+	fmt.Println(total)
+}
