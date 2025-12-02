c8e71ff0a5956198ab62a874166d3a7ce6815d50
James O'Brien <jobrienapps@gmail.com>
2025-12-01 20:25:11 -0500

    2025 Day 1
    Still haven't got part two yet

---
+++ 2025/01.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("01.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	// partOne(scanner)
+	partTwo(scanner)
+}
+
+func partOne(scanner *bufio.Scanner) {
+	dial := 50
+	total := 0
+	passesZero := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		clicks, clicksErr := strconv.Atoi(line[1:])
+
+		if clicksErr == nil {
+			// If it's over 100, reduce it because the dial only has 100 positions
+			if clicks >= 100 {
+				passesZero = passesZero + (clicks / 100)
+				clicks = clicks % 100
+			}
+
+			// Rotate the dial
+			switch line[0:1] {
+			case "R":
+				dial = dial + clicks
+			case "L":
+				dial = dial - clicks
+			}
+
+			// Validate the dial position
+			if dial > 100 {
+				dial = dial - 100
+				passesZero = passesZero + 1
+			} else if dial < 0 {
+				dial = dial + 100
+				passesZero = passesZero + 1
+			} else if dial == 100 {
+				dial = 0
+			}
+
+			// If we finish on 0, increase total
+			if dial == 0 {
+				passesZero = passesZero + 1
+				total = total + 1
+			}
+		}
+	}
+
+	println("Part 1: ", total)
+	println("Part 2: ", passesZero)
+}
+
+/**
+* For some reason my approach to get the first answer isn't working for the second part.
+* So I'll just brute force it by iterating each click one by one.
+ */
+func partTwo(scanner *bufio.Scanner) {
+	dial := 50
+	total := 0
+	passesZero := 0
+	direction := 1
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		clicks, clicksErr := strconv.Atoi(line[1:])
+
+		if clicksErr == nil {
+			// Rotate the dial
+			switch line[0:1] {
+			case "R":
+				direction = 1
+			case "L":
+				direction = -1
+			}
+
+			for i := 0; i < clicks; i++ {
+				dial = dial + direction
+
+				if dial > 99 {
+					dial = 0
+					passesZero = passesZero + 1
+				} else if dial < 0 {
+					dial = 99
+					passesZero = passesZero + 1
+				}
+
+				// If we're about to finish and we're on 0, count it
+				if dial == 0 && i == clicks-1 {
+					passesZero = passesZero + 1
+				}
+			}
+
+			// If we finish on 0, increase total
+			if dial == 0 {
+				total = total + 1
+			}
+		}
+	}
+
+	println("Part 1: ", total)
+	println("Part 2:", passesZero)
+}
