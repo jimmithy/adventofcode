6719d10c9f91f0e22d6ec251f358a4c35470340d
James O'Brien <james.obrien@hinge.co>
2025-12-04 12:38:10 -0500

    Day Four Part Two

--- 2025/04.go
+++ 2025/04.go
@@ -36,14 +36,11 @@ func main() {
 		row += 1
 	}
 
-	println("Part One:", dayFour(grid))
-
+	println("Part One:", dayFourPartOne(grid))
+	println("Part Two:", dayFourPartTwo(grid))
 }
 
-// 138 - @@.@..@@.@.@@@@@@@..@.@@@@..@@@.@.@@@@@@@@..@@..@.@@@.@@@@...@@.@....@@@@@@@.@..@@@@@@.@@@@@.@@.@@@@@@@@@@.@.@@@@.@@@@@@..@@@.@@.@.@@.@@@..
-// 139 - @.@.@@.@@@@@@@@@.@@@@.@..@@@.@..@@@@.@@.@@@@@..@@@@..@...@@@@@@@@......@.@@@@@@@@...@.@@@@.@..@@@.@@@@@@.@@@..@@@@@@@@@@@@.@@.@@@@.@@.@@@@.
-
-func dayFour(grid [][]int) int {
+func dayFourPartOne(grid [][]int) int {
 	total := 0
 	totalRows := len(grid)
 
@@ -56,81 +53,123 @@ func dayFour(grid [][]int) int {
 				continue
 			}
 
-			count := 0
-			hasPreviousRow := rowIndex > 0
-			hasLeft := colIndex > 0
-			hasRight := colIndex < rowLength-1
-			hasNextRow := rowIndex < totalRows-1
-
-			// Search previous row
-			if hasPreviousRow {
-				previousRow := grid[rowIndex-1]
-				if hasLeft {
-					topLeft := previousRow[colIndex-1]
-					if topLeft == 1 {
-						count += 1
-					}
-				}
-
-				above := previousRow[colIndex]
-				if above == 1 {
-					count += 1
-				}
-
-				if hasRight {
-					topRight := previousRow[colIndex+1]
-
-					if topRight == 1 {
-						count += 1
-					}
-				}
-			}
-
-			// Search left and right positions
-			if hasLeft {
-				left := row[colIndex-1]
-				if left == 1 {
-					count += 1
-				}
-			}
-
-			if hasRight {
-				right := row[colIndex+1]
-				if right == 1 {
-					count += 1
-				}
-			}
-
-			// search next row
-			if hasNextRow {
-				nextRow := grid[rowIndex+1]
-
-				if hasLeft {
-					bottomLeft := nextRow[colIndex-1]
-					if bottomLeft == 1 {
-						count += 1
-					}
-				}
-
-				below := nextRow[colIndex]
-				if below == 1 {
-					count += 1
-				}
-
-				if hasRight {
-					bottomRight := nextRow[colIndex+1]
-					if bottomRight == 1 {
-						count += 1
-					}
-				}
-			}
-
 			// finally confirm the count is fewer than four
-			if count < 4 {
+			if hasFourAdjacent(grid, rowIndex, colIndex, totalRows, rowLength) {
 				total += 1
 			}
 		}
 	}
 
 	return total
+}
+
+// 138 - @@.@..@@.@.@@@@@@@..@.@@@@..@@@.@.@@@@@@@@..@@..@.@@@.@@@@...@@.@....@@@@@@@.@..@@@@@@.@@@@@.@@.@@@@@@@@@@.@.@@@@.@@@@@@..@@@.@@.@.@@.@@@..
+// 139 - @.@.@@.@@@@@@@@@.@@@@.@..@@@.@..@@@@.@@.@@@@@..@@@@..@...@@@@@@@@......@.@@@@@@@@...@.@@@@.@..@@@.@@@@@@.@@@..@@@@@@@@@@@@.@@.@@@@.@@.@@@@.
+
+func dayFourPartTwo(grid [][]int) int {
+	total := 0
+	totalRows := len(grid)
+	continueLooping := true
+	mutableGrid := grid
+
+	// Loop until we run out of toilet rolls to remove
+	for continueLooping {
+		hasChanges := false
+
+		for rowIndex, _ := range grid {
+			rowLength := len(grid[rowIndex])
+
+			for colIndex := 0; colIndex < rowLength; colIndex++ {
+				// Only search around toilet roles
+				if grid[rowIndex][colIndex] != 1 {
+					continue
+				}
+
+				// finally confirm the count is fewer than four
+				if hasFourAdjacent(grid, rowIndex, colIndex, totalRows, rowLength) {
+					total += 1
+					mutableGrid[rowIndex][colIndex] = 0
+					hasChanges = true
+				}
+			}
+		}
+
+		continueLooping = hasChanges
+		grid = mutableGrid
+	}
+
+	return total
+}
+
+func hasFourAdjacent(grid [][]int, rowIndex int, colIndex int, totalRows int, rowLength int) bool {
+	count := 0
+	hasPreviousRow := rowIndex > 0
+	hasLeft := colIndex > 0
+	hasRight := colIndex < rowLength-1
+	hasNextRow := rowIndex < totalRows-1
+
+	// Search previous row
+	if hasPreviousRow {
+		previousRow := grid[rowIndex-1]
+		if hasLeft {
+			topLeft := previousRow[colIndex-1]
+			if topLeft == 1 {
+				count += 1
+			}
+		}
+
+		above := previousRow[colIndex]
+		if above == 1 {
+			count += 1
+		}
+
+		if hasRight {
+			topRight := previousRow[colIndex+1]
+
+			if topRight == 1 {
+				count += 1
+			}
+		}
+	}
+
+	// Search left and right positions
+	if hasLeft {
+		left := grid[rowIndex][colIndex-1]
+		if left == 1 {
+			count += 1
+		}
+	}
+
+	if hasRight {
+		right := grid[rowIndex][colIndex+1]
+		if right == 1 {
+			count += 1
+		}
+	}
+
+	// search next row
+	if hasNextRow {
+		nextRow := grid[rowIndex+1]
+
+		if hasLeft {
+			bottomLeft := nextRow[colIndex-1]
+			if bottomLeft == 1 {
+				count += 1
+			}
+		}
+
+		below := nextRow[colIndex]
+		if below == 1 {
+			count += 1
+		}
+
+		if hasRight {
+			bottomRight := nextRow[colIndex+1]
+			if bottomRight == 1 {
+				count += 1
+			}
+		}
+	}
+
+	return count < 4
 }
